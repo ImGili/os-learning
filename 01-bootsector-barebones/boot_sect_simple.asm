2ddec4932b6cd5d7343cee25b5ec82249a0dbc14
xhguleixin123 <18017158729@163.com>
2022-05-08 12:30:50 +0800

    ✨ feat: 添加代码注释

--- 01-bootsector-barebones/boot_sect_simple.asm
+++ 01-bootsector-barebones/boot_sect_simple.asm
@@ -1,9 +1,10 @@
 ; 无限循环 (e9 fd ff)
+; 这里就是 $
 loop:
     jmp loop 
 
 ; times是重复操作的意思，$-$$是当前行命令到程序开始的程序距离，db是指定字节
-times 510-($-$$) db 0
+times 510-($-$$) db 0 ; 这里就是 $$
 ; Magic number
 ; bios检查开机引导程序的标志
 dw 0xaa55 
